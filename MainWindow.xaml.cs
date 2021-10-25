a7b24a88c29e7f9b28412309711d838d68cba3ab
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-10-26 00:21:18 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DeviceId;
+using DeviceId.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IDGetter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbID.Text = GetIdHash();
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(tbID.Text);
+        }
+        string GetIdHash()
+        {
+            return new DeviceIdBuilder().AddProcessorId().AddMotherboardSerialNumber().AddComponent(new DeviceIdComponent("Telegram", "Telegram")).ToString();
+        }
+    }
+}
